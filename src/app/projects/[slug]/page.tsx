69e96f2f6b50513bd8749e1772f18dc45cca560b
Diana Vitanyi <>
2025-02-26 21:28:16 +0100

    portable text implementation

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { PROJECT_QUERY } from "@/sanity/lib/queries";
 import { Project } from "@/sanity/types";
+import { PortableText } from "@portabletext/react";
 
 export default async function ProjectPage({
   params,
@@ -13,13 +14,16 @@ export default async function ProjectPage({
   const projectRes: Project[] = await client.fetch(PROJECT_QUERY, queryParams);
 
   const [project] = projectRes;
-  const { title, summary, _createdAt } = project;
+  const { title, summary, _createdAt, description } = project;
 
   return (
     <section className="section container">
       <h1>{title}</h1>
       <p>{summary}</p>
       <p>{formatDate(_createdAt)}</p>
+      <div className="portabletext">
+        {description && <PortableText value={description} />}
+      </div>
     </section>
   );
 }
