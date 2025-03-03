5ba50424906354af8314ed0219d730c010960c4c
Diana Vitanyi <>
2025-03-03 12:08:06 +0100

    projectsearchquery fix to fit projects[] as result

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,7 +1,11 @@
 import { defineQuery } from "next-sanity";
 
 export const PROJECTS_SEARCH_QUERY = defineQuery(
-  `*[_type=='project' && !defined($search) || title match $search || description match $search || summary match $search] | order(_createdAt desc)`,
+  `*[
+  _type == "project" &&
+  (!defined($search) || title match $search || description match $search || summary match $search)
+]
+| order(_createdAt desc)`,
 );
 export const PROJECTS_QUERY = defineQuery(
   `*[_type=='project'] | order(_createdAt desc)`,
